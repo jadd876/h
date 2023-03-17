b529081b44b0c62eb7a3d7cab04b0aadf9e450cf
jadd876 <128140484+jadd876@users.noreply.github.com>
2023-03-17 10:09:19 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("Hello!")
+        basic.showString("lilly ")
+        basic.showIcon(IconNames.Ghost)
+        basic.showIcon(IconNames.Heart)
+    }
+})
